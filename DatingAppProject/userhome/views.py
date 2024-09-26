5324b0e3544840a6e4477f25e3731e3fca630ff1
prajina kk <prajinakk7@gmail.com>
2024-09-26 17:11:34 +0900

    story-section backend added

--- DatingAppProject/userhome/views.py
+++ DatingAppProject/userhome/views.py
@@ -12,7 +12,7 @@ class HomeView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-        
+        context['all_user'] = User.objects.filter(is_superuser=False)
         context['profiles'] = User.objects.filter(location=user.location.id)[1:3]
         context['designation_profiles'] = User.objects.filter(designation=user.designation)[1:3]
         context['qualification_profiles'] = User.objects.filter(qualification=user.qualification)[1:3]
